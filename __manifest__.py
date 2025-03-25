714a0b1b81b70e8be68408859f706b342be1ef0e
Gaural30 <gauralmakwana.skyscendbs@gmail.com>
2025-03-25 11:42:25 +0530

    vehicle model added

--- __manifest__.py
+++ __manifest__.py
@@ -5,11 +5,13 @@
     'auther': 'Gaural Makwana',
     'depends':['base'],
     'data' : [
-        'security/machine_security.xml',
+        'security/garage_security.xml',
         'security/ir.model.access.csv',
+        'views/vehicle_view.xml',
     ],
+    'sequence': 10,
     'auto_install': False,
     'installable' : True,
     'application' : True,
-    'license' : 'LGPL-3'
+    'license' : 'LGPL-3',
 }
