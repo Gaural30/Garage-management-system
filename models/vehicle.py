1d1548cc58480d63b03c329262480255c60590b4
Gaural30 <gauralmakwana.skyscendbs@gmail.com>
2025-03-25 12:04:49 +0530

    [ADD] Index in date field

--- models/vehicle.py
+++ models/vehicle.py
@@ -12,7 +12,7 @@ class GarageVehicle(models.Model):
     model = fields.Char(string='Model', help="Model of vehicle")
     year = fields.Integer(string='Year' , help="Manufacturing Year")
     km = fields.Float(string="Kilometers", help="Current Kilometers")
-    date=fields.Date(string="Date", default=fields.Date.today(), help="Today's Date")
+    date=fields.Date(string="Date", default=fields.Date.today(), help="Today's Date" , index=True)
     time = fields.Datetime(string="DateTime", default=fields.Datetime.now(), help="Current Date and Time")
     service_type = fields.Selection(selection=[('free','Free'),('paid','Paid')], string='Srevice Type', help="Service type")
     # gender = fields.Selection(selection=[('male', 'Male'), ('female', 'Female')], string='Gender')
