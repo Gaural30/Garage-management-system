4c061a57442d8f4850c5ca6d4d8bb001dac4c4ce
Gaural30 <gauralmakwana.skyscendbs@gmail.com>
2025-03-25 12:21:11 +0530

    [ADD] In the float field add 3 digits after decimal point.

--- models/vehicle.py
+++ models/vehicle.py
@@ -11,7 +11,7 @@ class GarageVehicle(models.Model):
     manufacturer = fields.Char(string='Manufacturer',help="Name of company")
     model = fields.Char(string='Model', help="Model of vehicle")
     year = fields.Integer(string='Year' , help="Manufacturing Year")
-    km = fields.Float(string="Kilometers", help="Current Kilometers")
+    km = fields.Float(string="Kilometers", help="Current Kilometers", digits=(7, 3))
     date=fields.Date(string="Date", default=fields.Date.today(), help="Today's Date" , index=True)
     time = fields.Datetime(string="DateTime", default=fields.Datetime.now(), help="Current Date and Time")
     service_type = fields.Selection(selection=[('free','Free'),('paid','Paid')], string='Srevice Type', help="Service type")
