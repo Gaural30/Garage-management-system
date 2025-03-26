2234bda26b6a443703aabe562ccffe5101367400
Gaural30 <gauralmakwana.skyscendbs@gmail.com>
2025-03-26 15:00:39 +0530

    ADD comment in Repair model

--- models/repair.py
+++ models/repair.py
@@ -1,6 +1,8 @@
 from odoo import models, fields
 
 
+# This model is for one2Many relation between Symptom and Vehicle
+
 class repair(models.Model):
     _name="garage.repair"
     _description = "This is for add one2many in vehiches"
