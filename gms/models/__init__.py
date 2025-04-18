5efe5ce2f25a03a0857d5cb293c63eb41c57de63
Gaural30 <gauralmakwana.skyscendbs@gmail.com>
2025-04-18 12:33:28 +0530

    [IMP]garage: sequence in __manifest__.py

--- gms/models/__init__.py
+++ gms/models/__init__.py
@@ -1,7 +1,10 @@
 from . import vehicle
-from . import customer
-from . import symptom
-from . import repair
-from . import jobcard
 from . import vehicle_company
+from . import symptom
+from . import jobcard
+from . import customer
+
+from . import repair
+
+
 from . import sparepart 
--- gms/models/sparepart.py
+++ gms/models/sparepart.py
@@ -9,10 +9,10 @@ class SpareParts(models.Model):
     name = fields.Char(string="Part Name", required=True)
     # part_number = fields.Char(string="Part Number", required=True, unique=True)
     product_id = fields.Many2one('product.product', string="Linked Product", required=True)
-    quantity = fields.Float(related='product_id.qty_available', string="Stock Quantity", readonly=True)
-    cost_price = fields.Float(related='product_id.standard_price', string="Cost Price", readonly=True)
-    sale_price = fields.Float(related='product_id.list_price', string="Sale Price", readonly=True)
-    location_id = fields.Many2one('stock.location', string="Stock Location")
+    quantity = fields.Float( string="Stock Quantity", readonly=True)
+    cost_price = fields.Float(string="Cost Price", readonly=True)
+    sale_price = fields.Float(string="Sale Price", readonly=True)
+    # location_id = fields.Many2one('stock.location', string="Stock Location")
 
 
     
